340d3e51503fc6b7497d72a72e70c53af44f3d7a
龚宇 <564542431@qq.com>
2022-01-05 23:20:18 +0800

    1、更新PodsGYCommon 2、适配PodsGYCommon

--- PhotoLibrary/GYCommon/Headers/GYCommonOCHeader.h
+++ PhotoLibrary/GYCommon/Headers/GYCommonOCHeader.h
@@ -13,23 +13,4 @@
 #define BS(blockSelf)  __block __typeof(&*self)blockSelf = self;
 #define SS(strongSelf) __strong __typeof(&*self)strongSelf = weakSelf;
 
-// block
-#ifndef dispatch_main_sync_safe
-#define dispatch_main_sync_safe(block)\
-if ([NSThread isMainThread]) {\
-block();\
-} else {\
-dispatch_sync(dispatch_get_main_queue(), block);\
-}
-#endif
-
-#ifndef dispatch_main_async_safe
-#define dispatch_main_async_safe(block)\
-if ([NSThread isMainThread]) {\
-block();\
-} else {\
-dispatch_async(dispatch_get_main_queue(), block);\
-}
-#endif
-
 #endif /* GYCommonOCHeader_h */
--- PhotoLibrary/GYiOS/Headers/GYiOSOCHeader.h
+++ PhotoLibrary/GYiOS/Headers/GYiOSOCHeader.h
@@ -11,4 +11,13 @@
 #define screenWidth (MAX(kScreenWidth, kScreenHeight))
 #define screenHeight (MIN(kScreenWidth, kScreenHeight))
 
+#ifndef dispatch_main_sync_safe
+#define dispatch_main_sync_safe(block)\
+    if (dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL) == dispatch_queue_get_label(dispatch_get_main_queue())) {\
+        block();\
+    } else {\
+        dispatch_sync(dispatch_get_main_queue(), block);\
+    }
+#endif
+
 #endif /* GYiOSOCHeader_h */
